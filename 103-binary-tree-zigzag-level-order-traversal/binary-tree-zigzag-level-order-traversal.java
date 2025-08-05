8860d7505306a460aaa1c65c06064c9cc56cd307
Karthikjatta <jattakarthik15@gmail.com>
2025-08-05 06:42:27 +0530

    Time: 1 ms (63.69%) | Memory: 42.2 MB (77.91%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,41 +15,31 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       
-        List<List<Integer>> ans =new ArrayList<>();
-         if(root==null) return ans;
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
         Deque<TreeNode> q=new LinkedList<>();
-        boolean flag=false;
         q.offer(root);
+        boolean rev=false;
         while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                if(flag==false){
+            int level=q.size();
+            LinkedList<Integer> currlev=new LinkedList<>();
+            for(int i=0;i<level;i++){
+                if(!rev){
                     TreeNode curr=q.pollFirst();
-                    temp.add(curr.val);
-                    if(curr.left!=null){
-                        q.addLast(curr.left);
-                    } 
-                    if(curr.right!=null){
-                        q.addLast(curr.right);
-                    }
-                    
-                }
-                else{
+                    currlev.add(curr.val);
+                    if(curr.left!=null) q.addLast(curr.left);
+                    if(curr.right!=null) q.addLast(curr.right);
+                }else{
                     TreeNode curr=q.pollLast();
-                    temp.add(curr.val);
-                    if(curr.right!=null){
-                        q.addFirst(curr.right);
-                    }
-                    if(curr.left!=null){
-                        q.addFirst(curr.left);
-                    }  
+                    currlev.add(curr.val);
+                    if(curr.right!=null) q.addFirst(curr.right);
+                    if(curr.left!=null) q.addFirst(curr.left);
                 }
+
             }
-            flag =!flag;
-            ans.add(temp);
+            rev=!rev;
+            res.add(new ArrayList(currlev));
         }
-        return ans;
+        return res;
     }
 }
