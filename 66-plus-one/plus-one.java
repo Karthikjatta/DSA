0d205c48de477cbb3102ec08c62ea65d504373d1
Karthikjatta <jattakarthik15@gmail.com>
2025-09-08 14:24:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (48.46%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits[digits.length-1]!=9){
+            digits[digits.length-1] +=1;
+            return digits;
+        }
+        int carry=1;
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]+carry==10){
+                digits[i]=0;
+                carry=1;
+            }
+            else{
+                digits[i]+=carry;
+                carry=0;
+            }
+        }
+        if(carry==1){
+            int[] ans=new int[digits.length+1];
+            ans[0]=1;
+            for(int i=1;i<ans.length;i++){
+                ans[i]=digits[i-1];
+            }
+            return ans;
+        }
+        return digits;
+    }
+}
