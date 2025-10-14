32a131c5f73e1bb9dedad54a9d744d31e3bf4a11
Karthikjatta <jattakarthik15@gmail.com>
2025-10-14 07:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (97.72%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,14 +14,10 @@
  * }
  */
 class Solution {
-    boolean helper(TreeNode p, TreeNode q){
-        if(p!=null&&q!=null){
-            if(p.val!=q.val) return false;
-           return helper(p.left,q.left) && helper(p.right,q.right);
-        }
-        return (p==null && q==null);
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-     return helper(p,q);
+            if(p==null&&q==null) return true;
+            if(p==null||q==null) return false;
+            if(p.val!=q.val) return false;
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
     }
 }
