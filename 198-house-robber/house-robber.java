202540acd0769b33a4211a1a88eddf3dacef0900
Karthikjatta <jattakarthik15@gmail.com>
2025-10-22 11:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (36.68%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp=new int[nums.length+1];
+        dp[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=Math.max(take,nottake);
+        }
+        return dp[nums.length-1];
+    }
+}
